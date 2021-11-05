a99db58b38121a09e1d66bab189028ae5b1d4806
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-05 23:49:24 +0100

    WIP admin and serializers files

--- EpicEvents_CRM/client/admin.py
+++ EpicEvents_CRM/client/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client
+
+#Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    """"""
+    list_display = ('first_name', 'last_name', )
