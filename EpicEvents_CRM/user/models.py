328b55a08f5f198ee442d012201ba4d0856f9e1d
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-05 17:54:17 +0100

    Wip on modeles files

--- EpicEvents_CRM/user/models.py
+++ EpicEvents_CRM/user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+class Team(models.Model):
+    """ create model team to add a role to the user"""
+    roles = [('sales', 'Sales'),
+             ('management', 'Management'),
+             ('support', 'Support')]
+    
+    role = models.CharField(max_length=100, choices=roles)
+    
+
+class User(models.Model):
+    """Create the user model"""
+    
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=50)
+    role = models.ForeignKey(to=Team)
