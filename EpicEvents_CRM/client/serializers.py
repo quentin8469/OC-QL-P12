ad58bec478b8a767fe28577d3b776cf501c396ea
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-05 22:38:44 +0100

    WIP serializer file

---
+++ EpicEvents_CRM/client/serializers.py
@@ -0,0 +1,10 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Client
+
+
+class ClientSerializer(serializers.ModelField):
+    """ Client serializer """
+    class Meta:
+        model = Client
+        fields = "__all__"
