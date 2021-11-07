a0058d1f8301242fdd160db289ffd2b66c517a9d
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-07 23:47:33 +0100

    WIP user model

--- EpicEvents_CRM/client/models.py
+++ EpicEvents_CRM/client/models.py
@@ -1,7 +1,28 @@
 from django.db import models
 from user.models import User
+from user.models import TeamUser
 
 # Create your models here.
+# class Client(models.Model):
+#     """ create model client"""
+#     first_name = models.CharField(max_length=25)
+#     last_name = models.CharField(max_length=25)
+#     email = models.EmailField(max_length=100)
+#     phone = models.CharField(max_length=20)
+#     mobile = models.CharField(max_length=20)
+#     company_name = models.CharField(max_length=250)
+#     date_create = models.DateTimeField(auto_now_add=True)
+#     date_update = models.DateTimeField(auto_now=True)
+#     sales_contact = models.ForeignKey(to=User,on_delete=models.CASCADE,null=True)
+    
+#     def __str__(self):
+#         return f"{self.company_name}, {self.last_name}, {self.first_name}"
+    
+#     class Meta:
+#         verbose_name = 'Client'
+#         verbose_name_plural = 'Clients'
+        
+
 class Client(models.Model):
     """ create model client"""
     first_name = models.CharField(max_length=25)
@@ -12,7 +33,7 @@ class Client(models.Model):
     company_name = models.CharField(max_length=250)
     date_create = models.DateTimeField(auto_now_add=True)
     date_update = models.DateTimeField(auto_now=True)
-    sales_contact = models.ForeignKey(to=User,on_delete=models.CASCADE,null=True)
+    sales_contact = models.ForeignKey(to=TeamUser,on_delete=models.CASCADE,null=True)
     
     def __str__(self):
         return f"{self.company_name}, {self.last_name}, {self.first_name}"
--- EpicEvents_CRM/user/models.py
+++ EpicEvents_CRM/user/models.py
@@ -1,28 +1,50 @@
+from _typeshed import Self
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Team(models.Model):
-    """ create model team to add a role to the user"""
-    roles = [('sales', 'Sales'),
-             ('management', 'Management'),
-             ('support', 'Support')]
+# class Team(models.Model):
+#     """ create model team to add a role to the user"""
+#     roles_choices = [('sales', 'Sales'),
+#              ('management', 'Management'),
+#              ('support', 'Support')]
     
-    role = models.CharField(max_length=100, choices=roles)
+#     role = models.CharField(max_length=50, choices=roles_choices)
     
 
-class User(models.Model):
-    """Create the user model"""
+# class User(models.Model):
+#     """Create the user model"""
     
-    first_name = models.CharField(max_length=25)
-    last_name = models.CharField(max_length=25)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=50)
-    role = models.ForeignKey(to=Team,on_delete=models.CASCADE,null=True )
+#     first_name = models.CharField(max_length=25)
+#     last_name = models.CharField(max_length=25)
+#     email = models.EmailField(max_length=100)
+#     password = models.CharField(max_length=50)
+#     role = models.ForeignKey(to=Team,on_delete=models.CASCADE,null=True )
+    
+#     def __str__(self):
+#         return f"{self.last_name}, {self.first_name}"
+    
+#     class Meta:
+#         verbose_name = 'User'
+#         verbose_name_plural = 'Users'
+
+
+class TeamUser(models.Model):
+    """ Create the team user model"""
+    roles_choices = [('sales', 'Sales'),
+                    ('management', 'Management'),
+                    ('support', 'Support')
+                    ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, choices=roles_choices)
     
     def __str__(self):
-        return f"{self.last_name}, {self.first_name}"
+        return f"{self.last_name}, {self.first_name}, {self.role}"
     
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+        
+    
