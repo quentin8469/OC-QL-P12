8afe93cf2afa355bc6252d6ed63754918a1cb17c
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-08 15:30:34 +0100

    WIP views file

--- EpicEvents_CRM/contract/views.py
+++ EpicEvents_CRM/contract/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializers import ContractSerializer
+from .models import Contract
+
 
 # Create your views here.
+
+class ContractViewset(viewsets.ModelViewSet):
+    """"""
+    
+    serializer_class = ContractSerializer
+    #permission_classes = [permissions.IsAuthenticated,]
+    #queryset = Contract.objects.all()
+    
+    def get_queryset(self):
+        return Contract.objects.all()
