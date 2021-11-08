0e55bc1bfd21b6bc37f0559e6096b7fc9ec23fe7
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-08 15:30:14 +0100

    WIP views file

--- EpicEvents_CRM/client/views.py
+++ EpicEvents_CRM/client/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializers import ClientSerializer
+from .models import Client
+
 
 # Create your views here.
+
+class ClientViewset(viewsets.ModelViewSet):
+    """"""
+    
+    serializer_class = ClientSerializer
+    #permission_classes = [permissions.IsAuthenticated,]
+    #queryset = Client.objects.all()
+    
+    def get_queryset(self):
+        return Client.objects.all()
