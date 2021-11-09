f5423fc9b41a7f6cfeba95ef8cd876bd44858e72
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-09 11:23:35 +0100

    Add serializers evenstatus

--- EpicEvents_CRM/event/serializers.py
+++ EpicEvents_CRM/event/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
-from .models import Event
+from .models import Event, EventStatus
 
 
 class EventSerializer(serializers.ModelSerializer):
     """ Event serializer """
     class Meta:
         model = Event
+        fields = "__all__"
+
+        
+class EventStatusSerializer(serializers.ModelSerializer):
+    """ Event serializer """
+    class Meta:
+        model = EventStatus
         fields = "__all__"
