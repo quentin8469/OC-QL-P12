a8060071911fd6b40543565dadfb5cbfb5d24156
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-09 10:10:32 +0100

    WIP models client

--- EpicEvents_CRM/client/models.py
+++ EpicEvents_CRM/client/models.py
@@ -1,27 +1,8 @@
 from django.db import models
-#from user.models import User
-from user.models import TeamUser
+from django.conf import settings
 
-# Create your models here.
-# class Client(models.Model):
-#     """ create model client"""
-#     first_name = models.CharField(max_length=25)
-#     last_name = models.CharField(max_length=25)
-#     email = models.EmailField(max_length=100)
-#     phone = models.CharField(max_length=20)
-#     mobile = models.CharField(max_length=20)
-#     company_name = models.CharField(max_length=250)
-#     date_create = models.DateTimeField(auto_now_add=True)
-#     date_update = models.DateTimeField(auto_now=True)
-#     sales_contact = models.ForeignKey(to=User,on_delete=models.CASCADE,null=True)
-    
-#     def __str__(self):
-#         return f"{self.company_name}, {self.last_name}, {self.first_name}"
-    
-#     class Meta:
-#         verbose_name = 'Client'
-#         verbose_name_plural = 'Clients'
-        
+
+# Create your models here.       
 
 class Client(models.Model):
     """ create model client"""
@@ -33,7 +14,7 @@ class Client(models.Model):
     company_name = models.CharField(max_length=250)
     date_create = models.DateTimeField(auto_now_add=True)
     date_update = models.DateTimeField(auto_now=True)
-    sales_contact = models.ForeignKey(to=TeamUser,on_delete=models.CASCADE,null=True)
+    sales_contact = models.ForeignKey(to=settings.AUTH_USER_MODEL,on_delete=models.CASCADE,null=True)
     
     def __str__(self):
         return f"{self.company_name}, {self.last_name}, {self.first_name}"
