497eab6233d074018c9e38f95f762ad51e20545b
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-09 10:16:42 +0100

    Add AUth_user_model in settinge file

--- EpicEvents_CRM/EpicEvents/settings.py
+++ EpicEvents_CRM/EpicEvents/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'user.TeamUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
