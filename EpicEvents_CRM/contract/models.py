798f3463c71893af7348f01557a9181b79395aab
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-09 10:11:50 +0100

    WIP models contract

--- EpicEvents_CRM/contract/models.py
+++ EpicEvents_CRM/contract/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from user.models import TeamUser
+from django.conf import settings
 from client.models import Client
 
 # Create your models here.
 class Contract(models.Model):
     """ Creat the contract objet"""
     
-    sales_contact = models.ForeignKey(to=TeamUser,on_delete=models.CASCADE,null=True)
+    sales_contact = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE,null=True)
     client = models.ForeignKey(to=Client,on_delete=models.CASCADE,null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_update = models.DateTimeField(auto_now=True)
