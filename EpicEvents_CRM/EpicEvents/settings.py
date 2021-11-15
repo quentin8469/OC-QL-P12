298ea8a78b0eb315c6d81cbdcc6d5ba016627536
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-15 14:05:31 +0100

    restore db squilte3 connection

--- EpicEvents_CRM/EpicEvents/settings.py
+++ EpicEvents_CRM/EpicEvents/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'EpicEvents.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'crmEpicEvents',
-        'USER': config('db_user'),
-        'PASSWORD': config('db_password'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
