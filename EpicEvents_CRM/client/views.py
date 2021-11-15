74a6ee9b8ea5a291e116f435ad2142598f543620
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-15 19:47:52 +0100

    Maj queryset for saler get

--- EpicEvents_CRM/client/views.py
+++ EpicEvents_CRM/client/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, permissions
 from .serializers import ClientSerializer
 from .models import Client
+from event.models import Event
+from .permission import ClientPermission
 
 
 # Create your views here.
@@ -9,8 +11,11 @@ class ClientViewset(viewsets.ModelViewSet):
     """"""
     
     serializer_class = ClientSerializer
-    #permission_classes = [permissions.IsAuthenticated,]
-    #queryset = Client.objects.all()
+    permission_classes = [permissions.IsAuthenticated, ClientPermission]
     
     def get_queryset(self):
-        return Client.objects.all()
+        user_team = self.request.user.role.role 
+        if user_team == 'sales':
+            return Client.objects.filter(sales_contact=self.request.user)
+        else:
+            return Client.objects.all()
