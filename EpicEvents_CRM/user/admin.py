fa6d99a65b377fefbe9e35fb590a29cb67297533
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-15 14:51:03 +0100

    Add methode save to hash the password

--- EpicEvents_CRM/user/admin.py
+++ EpicEvents_CRM/user/admin.py
@@ -9,6 +9,14 @@ class TeamUserForm(forms.ModelForm):
     """ change admin interface"""
     class Meta:
         fields = ('username','first_name', 'last_name', 'email', 'password', 'role', 'is_staff' )
+        
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(TeamUserForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
 
 
 @admin.register(TeamUser)
@@ -16,7 +24,7 @@ class TeamUserAdmin(admin.ModelAdmin):
     """"""
     ordering = ['role']
     list_display = ('username','first_name', 'last_name', 'email', 'role', 'id', 'is_staff')
-    #form = TeamUserForm
+    form = TeamUserForm
 
 
 admin.site.register(Team)
