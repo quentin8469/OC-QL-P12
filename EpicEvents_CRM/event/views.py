6f82ec1815b2f6ed5addbd202f1fc7c9c68e8790
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-16 14:03:24 +0100

    WIP on get_queryset

--- EpicEvents_CRM/event/views.py
+++ EpicEvents_CRM/event/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, permissions
 from django.shortcuts import get_object_or_404
 
+from .permission import EventPermission
+
 from .serializers import EventSerializer
 from .models import Event
 from client.models import Client
@@ -13,9 +15,15 @@ class EventViewset(viewsets.ModelViewSet):
     """"""
     
     serializer_class = EventSerializer
-    #permission_classes = [permissions.IsAuthenticated,]
+    permission_classes = [permissions.IsAuthenticated, EventPermission]
     #queryset = Event.objects.all()
     
     def get_queryset(self):
-        client = get_object_or_404(Client, pk=self.kwargs['client_id'])
-        return Event.objects.filter(client=client)
+        user_team = self.request.user.role.role 
+        if user_team == 'support':
+            return Event.objects.filter(support_contact=self.request.user)
+        elif user_team == 'sales':
+            client = get_object_or_404(Client, pk=self.kwargs['client_id'])
+            return client.event_set.all()
+        else:
+            return Event.objects.all()
