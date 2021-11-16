8ac99796d1385cd50bb87afe1727567699fea73c
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-16 18:22:46 +0100

    Add python decouple for postgres connxion

--- EpicEvents_CRM/EpicEvents/settings.py
+++ EpicEvents_CRM/EpicEvents/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'EpicEvents.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bddEpicEvents',
+        'USER': config('db_user'),
+        'PASSWORD': config('db_password'),
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
