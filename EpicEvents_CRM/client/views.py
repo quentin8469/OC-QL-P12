0073389a233545a9eee14e5d2d25b8dec9d3c8df
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-30 13:19:31 +0100

    Add objects filter

--- EpicEvents_CRM/client/views.py
+++ EpicEvents_CRM/client/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, permissions
+from django.http.response import Http404
 from .serializers import ClientSerializer
 from .models import Client
 from event.models import Event
@@ -19,13 +20,26 @@ class ClientViewset(viewsets.ModelViewSet):
     def get_queryset(self):
         user_team = self.request.user.role.role 
         if user_team == 'sales':
-            return Client.objects.filter(sales_contact=self.request.user)
+            queryset = Client.objects.filter(sales_contact=self.request.user)
         elif user_team == 'support':
             event = Event.objects.filter(support_contact=self.request.user)
             id_client = event.values_list('client', flat=True)
-            return Client.objects.filter(pk__in=id_client )
+            queryset = Client.objects.filter(pk__in=id_client )
         else:
             return Client.objects.all()
         
+        client_lastname = self.request.query_params.get('name')
+        client_email = self.request.query_params.get('email')
+        
+        if client_lastname is not None:
+            queryset = queryset.filter(last_name=client_lastname)
+        if client_email is not None:
+            queryset = queryset.filter(email=client_email)
+        logger.warning('bob')     
+        return queryset
+        
     def perform_create(self, serializer):
-        serializer.save(sales_contact=self.request.user)
+        try:
+            serializer.save(sales_contact=self.request.user)
+        except:
+            logger.info('bad')
